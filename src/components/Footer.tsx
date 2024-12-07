702ee558ffae94b68fd033b708466966e3af7d0f
Alikuoff <alikuloff01@gmail.com>
2024-12-07 08:57:34 +0200

    footerni ozroq ozgartirdim

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,26 +2,26 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-4 mt-10">
         <div className="max-w-7xl mx-auto text-center">
-            <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-16 text-center animate-fade-in">
-                WEB SAHIIFAMGA XUSH KELIBSIZ<span className="text-purple-600">.</span>
-                </h1>
+            <h4 className="text-xl md:text-5xl lg:text-6xl font-bold mb-5 text-center animate-fade-in">
+                Web sahifamga xush kelibsiz<span className="text-purple-600">.</span>
+                </h4>
             <p>&copy; {new Date().getFullYear()} Aliqulov Azizjon. Barcha huquqlar himoyalangan.</p>
             <p className="mt-2">Saytning asosiy maqsadi foydali ma'lumotlar, bilim va tajribalarni almashish, shuningdek, professional rivojlanishga hissa qo'shishdan iborat.</p>
-            <div className="mt-4 flex justify-center space-x-4">
+            <div className="mt-4 flex flex-wrap justify-center space-x-4">
                 <a href="https://www.facebook.com/alikulovazizjon" target="_blank" rel="noopener noreferrer" className="flex items-center text-gray-300 hover:text-gray-100">
-                    <i className="fab fa-facebook-square mr-2 text-blue-500" aria-hidden="true"></i> Facebook
+                    <i className="fab fa-facebook-square mr-2 text-blue-500" aria-hidden="true">Facebook</i> 
                 </a>
                 <a href="https://www.instagram.com/alikulovazizjon" target="_blank" rel="noopener noreferrer" className="flex items-center text-gray-300 hover:text-gray-100">
-                    <i className="fab fa-instagram mr-2 text-pink-500" aria-hidden="true"></i> Instagram
+                    <i className="fab fa-instagram mr-2 text-pink-500" aria-hidden="true">Instagram</i>
                 </a>
                 <a href="https://www.linkedin.com/in/alikulovazizjon" target="_blank" rel="noopener noreferrer" className="flex items-center text-gray-300 hover:text-gray-100">
-                    <i className="fab fa-linkedin mr-2 text-blue-700" aria-hidden="true"></i> LinkedIn
+                    <i className="fab fa-linkedin mr-2 text-blue-700" aria-hidden="true">LinkedIn</i> 
                 </a>
                 <a href="https://www.twitter.com/alikulovazizjon" target="_blank" rel="noopener noreferrer" className="flex items-center text-gray-300 hover:text-gray-100">
-                    <i className="fab fa-twitter-square mr-2 text-blue-400" aria-hidden="true"></i> Twitter
+                    <i className="fab fa-twitter-square mr-2 text-blue-400" aria-hidden="true">Twitter</i> 
                 </a>
                 <a href="https://www.github.com/alikulovazizjon" target="_blank" rel="noopener noreferrer" className="flex items-center text-gray-300 hover:text-gray-100">
-                    <i className="fab fa-github mr-2 text-gray-600" aria-hidden="true"></i> GitHub
+                    <i className="fab fa-github mr-2 text-gray-400" aria-hidden="true">GitHub</i> 
                 </a>
             </div>
             </div>
